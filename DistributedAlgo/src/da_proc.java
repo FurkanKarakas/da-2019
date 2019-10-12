de9f3999e894a888ee41c2baa76db1b58b0256fc
Henrik Milan <henrik.milan@aalto.fi>
2019-10-12 13:10:15 +0200

    Added throws to the main function.

--- DistributedAlgo/src/da_proc.java
+++ DistributedAlgo/src/da_proc.java
@@ -18,39 +18,33 @@ public class da_proc {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, UnknownHostException, IOException {
         if (args.length > 0) {
             int n = Integer.parseInt(args[0]);
             String fileName = args[1];
 
             File membership = new File(fileName);
-            Scanner sc;
-            try {
-                sc = new Scanner(membership);
-                
-                // Parse first line from membership file that has total numbers of processes
-                Integer.parseInt(sc.nextLine());
+            Scanner sc = new Scanner(membership);
+ 
+            // Parse first line from membership file that has total numbers of processes
+            Integer.parseInt(sc.nextLine());
 
-                while (sc.hasNextLine()) {
-                    String[] params = sc.nextLine().trim().split(" ");
-                    if (Integer.parseInt(params[0]) == n) {
-                    	InetAddress piAddr = InetAddress.getByName("127.0.0.1");
-                    	InetAddress pjAddr = InetAddress.getByName("127.0.0.1");
+            while (sc.hasNextLine()) {
+                String[] params = sc.nextLine().trim().split(" ");
+                if (Integer.parseInt(params[0]) == n) {
+                	InetAddress piAddr = InetAddress.getByName("127.0.0.1");
+                	InetAddress pjAddr = InetAddress.getByName("127.0.0.1");
 
-                    	Process pi = new Process(piAddr, Integer.parseInt(params[2]), 12001);
-                        Process pj = new Process(pjAddr, 2, 12002);
+                	Process pi = new Process(piAddr, Integer.parseInt(params[2]), 12001);
+                    Process pj = new Process(pjAddr, 2, 12002);
 
-                        PerfectLinks pl = new PerfectLinks(pi, pj);
-                        pl.sendMessage("Hey pj");
-                        //pl.receiveMessge();
-                        
-                        
-                    }
+                    PerfectLinks pl = new PerfectLinks(pi, pj);
+                    pl.sendMessage("Hey pj");
+                    
+                    
                 }
-            } catch (Exception e) {
-                System.out.println(e);
             }
-
+            sc.close();
         }
     }
 
